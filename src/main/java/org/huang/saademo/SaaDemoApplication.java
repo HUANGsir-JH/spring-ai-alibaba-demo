d8d2adbfff23c561b9b956c3195b6e3cfd5e955d
HUANGsir-JH <huangsirjh2005@gmail.com>
2026-01-28 21:02:56 +0800

    init

---
+++ src/main/java/org/huang/saademo/SaaDemoApplication.java
@@ -0,0 +1,13 @@
+package org.huang.saademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaDemoApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SaaDemoApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/org/huang/saademo/SaaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.huang.saademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaaDemoApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
